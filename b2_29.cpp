c317f591754483fe21da4d2ae55b68b9a2f2b9d7
huynh le my huyen <hlmhhuyen.4112003@gmail.com>
2024-03-11 11:37:51 +0700

    reslove conflict

---
+++ b2_29.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cout << "tao branch feature-new"<<endl;
+	cout << "tao branch feature-new ------" << endl;
+	cout << "them 1 thay doi vao branch featre-new o local" << endl;
+	cout << "add 1 dong o main, day len github-- main chinh dong nay";
+	return 0;
+}
